5b788fb879a7ca86a42d45af23a185179bdd1177
Matheus Macedo <matheusg3.201168@gmail.com>
2022-06-20 18:34:25 -0300

    arquivos da quest JS basico

---
+++ index.js
@@ -0,0 +1,39 @@
+// SOMA
+function soma(number1, number2){
+    let soma = number1 + number2
+    return 'A soma dos valores é ' + soma
+}
+
+let resultadoDaSoma = soma(10, 5)
+
+console.log(resultadoDaSoma)
+
+//SUBTRAÇÃO
+function subtracao(number1, number2){
+    let subtracao = number1 - number2
+    return 'A soma dos valores é ' + subtracao
+}
+
+let resultadoDaSubtracao = subtracao(15, 10)
+
+console.log(resultadoDaSubtracao)
+
+//DIVISÃO
+function divisao(number1, number2){
+    let divisao = number1 / number2
+    return 'A soma dos valores é ' + divisao
+}
+
+let resultadoDaDivisao = divisao(4, 2)
+
+console.log(resultadoDaDivisao)
+
+//MUTIPLICAÇÃO
+function mutiplicacao(number1, number2){
+    let mutiplicacao = number1 * number2
+    return 'A soma dos valores é ' + mutiplicacao
+}
+
+let resultadoDaMutiplicacao = mutiplicacao(2, 10)
+
+console.log(resultadoDaMutiplicacao)
